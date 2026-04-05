5efb2937c362dddbff6c8d8e3ecfca7522e97080
bevanwsjones <bevanwsjones@gmail.com>
2026-04-05 15:38:19 +0200

    - moved draw_test out of unit tests and to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use strelitzia::trees::kd_tree::KDTree;
+use strelitzia::multiarray::Vector;
+
 fn main() {
     println!("Strelitzia computational physics library.");
     println!("Use as a library: `use strelitzia::prelude::*;`");
+
+    draw_test();
+}
+
+fn draw_test() {
+    let points: Vec<Vector<i32, 2>> = vec![
+        Vector::<i32, 2>::new(5, -2),
+        Vector::<i32, 2>::new(1, -4),
+        Vector::<i32, 2>::new(3, 0),
+    ];
+    let tree = KDTree::new().build(&points, 3);
+    tree.draw("kd_tree.svg").unwrap();
 }
--- src/trees/kd_tree.rs
+++ src/trees/kd_tree.rs
@@ -330,17 +330,6 @@ mod tests {
     }
 
     #[test]
-    fn draw_test() {
-        let points: Vec<Vector<i32, 2>> = vec![
-          Vector::<i32, 2>::new(5, -2),
-          Vector::<i32, 2>::new(1, -4),
-          Vector::<i32, 2>::new(3, 0),
-        ];
-        let tree = KDTree::new().build(&points, 3);
-        tree.draw("kd_tree.svg").unwrap();
-    }
-
-    #[test]
     fn third_depth_test() {
         let points: Vec<Vector<i32, 2>> = vec![
           Vector::<i32, 2>::new(5, -2),
