2ff0d6b57699ec197cb38c4e521e16d1e5e3c48c
bevanwsjones <bevanwsjones@gmail.com>
2026-05-27 23:12:21 +0100

    laid out types

--- src/b_rep/body.rs
+++ src/b_rep/body.rs
@@ -1,6 +1,23 @@
 
 use crate::multiarray::Vector3;
 
+// notes: 
+//  pages.mtu.edu/~shene/COURSES/cs3621/NOTES/
+//  developer.rhino3d.com/guides/general/essential-mathematics/parametric-curves-surfaces/
+//  https://opencascade.blogspot.com/
+
+enum CurveType {
+    Line,
+    Circle,
+    Nurbs,
+}
+
+enum SurfaceType {
+    Plane,
+    Cylinder,
+    Nurbs
+}
+
 struct Edge {
   pub vertices: Vec<Vector3>,
 }
@@ -12,4 +29,5 @@ struct Face {
 struct Body{
   pub faces: Vec<Face>,
 }
+
 
