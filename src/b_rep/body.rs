c223d3c10141ab8b9cc60d8f74b747144cf07e1a
bevanwsjones <bevanwsjones@gmail.com>
2026-06-01 22:05:31 +0100

    - create_loop added

--- src/b_rep/body.rs
+++ src/b_rep/body.rs
@@ -1,7 +1,5 @@
 
-use std::fs::File;
-use std::io::{self, BufWriter, Write};
-use std::path::Path;
+use std::fmt::format;
 
 use crate::multiarray::Vector3;
 
@@ -27,6 +25,10 @@ struct Edge {
     pub vertices: [usize; 2],
 }
 
+struct Loop {
+    pub edges: Vec<usize>,
+}
+
 struct Face {
     pub edges: Vec<Edge>,
 }
@@ -34,6 +36,7 @@ struct Face {
 struct Body {
     pub vertices: Vec<Vector3>,
     pub edges: Vec<Edge>,
+    pub loops: Vec<usize>,
     pub faces: Vec<Face>,
 }
 
@@ -46,6 +49,36 @@ impl Body {
     pub fn add_edge(mut self, vertex_0: usize, vertex_1: usize) -> usize {
         self.edges.push(Edge{vertices: [vertex_0, vertex_1]});
         self.edges.len() - 1
+    }
+
+    pub fn create_loop(mut self, edges: &Vec<usize>) -> Result<usize, String> {
+
+        if edges.is_empty() {
+            return Err("No edges provided.".to_string());
+        }
+        if edges.len() < 2 {
+            return Err(format!("Loop requires two edges, got {}.", edges.len()));
+        }
+
+        let mut new_loop = Loop{ edges: Vec::new() };
+        new_loop.edges.reserve(edges.len());
+        for (i_edge, i_next_edge) in edges.iter().zip(edges.iter().cycle().skip(1)) {
+            let edge = self.edges.get(*i_edge).ok_or( format!("Index {} not in range [0, {}).", *i_edge, self.edges.len()))?;
+            let next_edge = self.edges.get(*i_next_edge).ok_or( format!("Index {} not in range [0, {}).", *i_next_edge, self.edges.len()))?;
+            
+            if edge.vertices[1] == next_edge.vertices[0] {
+                new_loop.edges.push(*i_edge);
+            }
+            else {
+                return Err(format!(
+                    "Edge {} (vertices [{}, {}]) does not connect to edge {} (vertices [{}, {}]).",
+                    *i_edge, edge.vertices[0], edge.vertices[1],
+                    *i_next_edge, next_edge.vertices[0], next_edge.vertices[1]
+                ));
+            }
+        }
+        self.loops.push(new_loop);
+        Ok(self.loops.len() - 1)
     }
 }
 
