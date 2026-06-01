ecb20862a8bd162088174336fe16909bf832d118
bevanwsjones <bevanwsjones@gmail.com>
2026-06-01 21:14:24 +0100

    - added first implementation of vertices and edges

--- src/b_rep/body.rs
+++ src/b_rep/body.rs
@@ -1,7 +1,11 @@
 
+use std::fs::File;
+use std::io::{self, BufWriter, Write};
+use std::path::Path;
+
 use crate::multiarray::Vector3;
 
-// notes: 
+// notes:
 //  pages.mtu.edu/~shene/COURSES/cs3621/NOTES/
 //  developer.rhino3d.com/guides/general/essential-mathematics/parametric-curves-surfaces/
 //  https://opencascade.blogspot.com/
@@ -15,19 +19,33 @@ enum CurveType {
 enum SurfaceType {
     Plane,
     Cylinder,
-    Nurbs
+    Nurbs,
 }
 
+
 struct Edge {
-  pub vertices: Vec<Vector3>,
+    pub vertices: [usize; 2],
 }
 
 struct Face {
-  pub edges: Vec<Edge>,
+    pub edges: Vec<Edge>,
 }
 
-struct Body{
-  pub faces: Vec<Face>,
+struct Body {
+    pub vertices: Vec<Vector3>,
+    pub edges: Vec<Edge>,
+    pub faces: Vec<Face>,
 }
 
+impl Body {
+    pub fn add_vertex(mut self, point: &Vector3) -> usize {
+        self.vertices.push(point.clone());
+        self.vertices.len() - 1
+    }
+
+    pub fn add_edge(mut self, vertex_0: usize, vertex_1: usize) -> usize {
+        self.edges.push(Edge{vertices: [vertex_0, vertex_1]});
+        self.edges.len() - 1
+    }
+}
 
